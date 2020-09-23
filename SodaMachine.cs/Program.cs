eeb73f396ec7d698b1fd442712a8e7018c67c055
Josh Grimaldi <69489227+mabejohnny@users.noreply.github.com>
2020-09-23 08:50:28 -0400

    Initial Commit

---
+++ SodaMachine.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SodaMachine.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
